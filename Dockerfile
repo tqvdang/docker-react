94230d96aa1fe6d8c89736b6794cda4109517921
Dang Tran <tqvdang@msn.com>
2020-11-27 15:02:08 -0800

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:alpine
+WORKDIR '/app'
+COPY package.json .
+RUN yarn install
+COPY . . 
+RUN yarn build
+
+FROM nginx
+COPY --from=0 /app/build /usr/share/nginx/html
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link 1', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 3', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
