5626cdfe3e98e343956187afb23bc19bf6306f4d
Dang Tran <tqvdang@msn.com>
2020-11-27 17:16:44 -0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Yo. Edit <code>src/App.js</code> and save to reload.
+         I was changed again!!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch
+          Learn React
         </a>
       </header>
     </div>
